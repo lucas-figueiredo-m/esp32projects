3f3f0d8d6a0f5a598bf7c168ff23626b42ca99b4
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2018-11-01 01:14:20 -0300

    Solved the stackoverflow and Exception unhandled problems

--- controlledPWM/main/blink.c
+++ controlledPWM/main/blink.c
@@ -56,27 +56,27 @@ static const adc_unit_t unit = ADC_UNIT_1;
 
 QueueHandle_t xQueueReadings;
 
-/*
-void blink_task(void *pvParameter)
+ledc_timer_config_t ledc_timer = 
 {
-     Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.
-    
-    gpio_pad_select_gpio(BLINK_GPIO);
-     Set the GPIO as a push/pull output 
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
-         Blink off (output low) 
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-         Blink on (output high) 
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}*/
+    .duty_resolution = LEDC_TIMER_10_BIT,  // resolution of PWM duty
+    .freq_hz = 20000,                      // frequency of PWM signal
+    .speed_mode = LEDC_HS_MODE,            // timer mode
+    .timer_num = LEDC_HS_TIMER             // timer index
+};
+    // Set configuration of timer0 for high speed channels
+
+ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = 
+{
+    {
+        .channel    = LEDC_HS_CH0_CHANNEL,
+        .duty       = 64,
+        .gpio_num   = LEDC_HS_CH0_GPIO,
+        .speed_mode = LEDC_HS_MODE,
+        .timer_sel  = LEDC_HS_TIMER
+    },
+
+};
+
 
 void adcReadTask(void *pvParameter)
 {
@@ -128,13 +128,13 @@ void adcReadTask(void *pvParameter)
 
             else
             {
-                printf("\nFeiled to post value\n");
+                printf("\nFailed to post value\n");
             }
         }
         //Convert adc_reading to voltage in mV
         uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
         printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(50/portTICK_PERIOD_MS);
     }
 }
 
@@ -143,104 +143,25 @@ void pwmControlTask(void *pvParameter)
     //int ch;
     uint32_t adcRead = 0;
 
-    ledc_timer_config_t ledc_timer = {
-        .duty_resolution = LEDC_TIMER_10_BIT,  // resolution of PWM duty
-        .freq_hz = 20000,                      // frequency of PWM signal
-        .speed_mode = LEDC_HS_MODE,            // timer mode
-        .timer_num = LEDC_HS_TIMER             // timer index
-    };
-    // Set configuration of timer0 for high speed channels
     ledc_timer_config(&ledc_timer);
-
-
-
-    ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
-        {
-            .channel    = LEDC_HS_CH0_CHANNEL,
-            .duty       = 64,
-            .gpio_num   = LEDC_HS_CH0_GPIO,
-            .speed_mode = LEDC_HS_MODE,
-            .timer_sel  = LEDC_HS_TIMER
-        },
-
-       /*{
-            .channel    = LEDC_HS_CH1_CHANNEL,
-            .duty       = 128,
-            .gpio_num   = LEDC_HS_CH1_GPIO,
-            .speed_mode = LEDC_HS_MODE,
-            .timer_sel  = LEDC_HS_TIMER
-        },
-
-        {
-            .channel    = LEDC_HS_CH2_CHANNEL,
-            .duty       = 256,
-            .gpio_num   = LEDC_HS_CH2_GPIO,
-            .speed_mode = LEDC_HS_MODE,
-            .timer_sel  = LEDC_HS_TIMER
-        },
-
-        {
-            .channel    = LEDC_HS_CH3_CHANNEL,
-            .duty       = 512,
-            .gpio_num   = LEDC_HS_CH3_GPIO,
-            .speed_mode = LEDC_HS_MODE,
-            .timer_sel  = LEDC_HS_TIMER
-        },
-
-        {
-            .channel    = LEDC_HS_CH4_CHANNEL,
-            .duty       = 1024,
-            .gpio_num   = LEDC_HS_CH4_GPIO,
-            .speed_mode = LEDC_HS_MODE,
-            .timer_sel  = LEDC_HS_TIMER
-        },
-
-        {
-            .channel    = LEDC_HS_CH5_CHANNEL,
-            .duty       = 2048,
-            .gpio_num   = LEDC_HS_CH5_GPIO,
-            .speed_mode = LEDC_HS_MODE,
-            .timer_sel  = LEDC_HS_TIMER
-        },*/
-
-    };
-/*
-    for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) 
-    {
-        ledc_channel_config(&ledc_channel[ch]);
-    }*/
-
-    // Initialize fade service.
-    //ledc_fade_func_install(0);
     ledc_channel_config(&ledc_channel[0]);
 
     while (1) 
     {
         if(xQueueReceive(xQueueReadings, &adcRead, 50/portTICK_PERIOD_MS))
         {
-            printf("\nReading Successfull\n");
+            printf("\nReading Successfull. New PWM: %d", adcRead);
             ledc_set_duty(ledc_channel[0].speed_mode, ledc_channel[0].channel, adcRead);
             ledc_update_duty(ledc_channel[0].speed_mode, ledc_channel[0].channel);
         }
-/*
-        printf("1. LEDC fade up to duty = %d\n", LEDC_TEST_DUTY);
-        for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
-            ledc_set_fade_with_time(ledc_channel[ch].speed_mode,
-                    ledc_channel[ch].channel, LEDC_TEST_DUTY, LEDC_TEST_FADE_TIME);
-            ledc_fade_start(ledc_channel[ch].speed_mode,
-                    ledc_channel[ch].channel, LEDC_FADE_NO_WAIT);
-        }
-        vTaskDelay(LEDC_TEST_FADE_TIME / portTICK_PERIOD_MS);*/
     }
-
 }
-
 
 void app_main(void)
 {
     xQueueReadings = xQueueCreate(10, sizeof(int));
-    xTaskCreate(&adcReadTask, "adcReadTask", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
-    xTaskCreate(&pwmControlTask, "pwmControlTask", 10000, NULL, 5, NULL);
+    xTaskCreate(&adcReadTask, "adcReadTask", 5000, NULL, 5, NULL);
+    xTaskCreate(&pwmControlTask, "pwmControlTask", 5000, NULL, 5, NULL);
 }
 
 // Guru Meditation Error: Core  1 panic'ed (LoadProhibited). Exception was unhandled.

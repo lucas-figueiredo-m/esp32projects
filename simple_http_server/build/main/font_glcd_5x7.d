179c5a7c8a6edd8598d1560ca0eecb47bd5ce200
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2018-12-05 02:32:57 -0200

    Tried to add the OLED module, but miserably failed. I'll jump this step, but I kept the files, libs, etc on de folder

---
+++ simple_http_server/build/main/font_glcd_5x7.d
@@ -0,0 +1,18 @@
+font_glcd_5x7.o: \
+ /opt/esp/esp32projects/simple_http_server/main/font_glcd_5x7.c \
+ /opt/esp/esp32projects/simple_http_server/main/include/fonts.h \
+ /opt/esp/esp-idf/components/newlib/include/stdint.h \
+ /opt/esp/esp-idf/components/newlib/include/machine/_default_types.h \
+ /opt/esp/esp-idf/components/newlib/include/sys/features.h \
+ /opt/esp/esp-idf/components/newlib/include/sys/_intsup.h
+
+/opt/esp/esp32projects/simple_http_server/main/include/fonts.h:
+
+/opt/esp/esp-idf/components/newlib/include/stdint.h:
+
+/opt/esp/esp-idf/components/newlib/include/machine/_default_types.h:
+
+/opt/esp/esp-idf/components/newlib/include/sys/features.h:
+
+/opt/esp/esp-idf/components/newlib/include/sys/_intsup.h:
+/opt/esp/esp32projects/simple_http_server/main/./font_glcd_5x7.c:
---
+++ simple_http_server/build/main/font_tahoma_8pt.d
@@ -0,0 +1,18 @@
+font_tahoma_8pt.o: \
+ /opt/esp/esp32projects/simple_http_server/main/font_tahoma_8pt.c \
+ /opt/esp/esp32projects/simple_http_server/main/include/fonts.h \
+ /opt/esp/esp-idf/components/newlib/include/stdint.h \
+ /opt/esp/esp-idf/components/newlib/include/machine/_default_types.h \
+ /opt/esp/esp-idf/components/newlib/include/sys/features.h \
+ /opt/esp/esp-idf/components/newlib/include/sys/_intsup.h
+
+/opt/esp/esp32projects/simple_http_server/main/include/fonts.h:
+
+/opt/esp/esp-idf/components/newlib/include/stdint.h:
+
+/opt/esp/esp-idf/components/newlib/include/machine/_default_types.h:
+
+/opt/esp/esp-idf/components/newlib/include/sys/features.h:
+
+/opt/esp/esp-idf/components/newlib/include/sys/_intsup.h:
+/opt/esp/esp32projects/simple_http_server/main/./font_tahoma_8pt.c:
